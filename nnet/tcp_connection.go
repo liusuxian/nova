cdd393e92d6f5dfb4d46353501879cbebbff75ad
liusuxian <382185882@qq.com>
2023-02-19 02:00:25 +0800

    接口定义

--- nnet/tcp_connection.go
+++ nnet/tcp_connection.go
@@ -2,15 +2,13 @@
  * @Author: liusuxian 382185882@qq.com
  * @Date: 2023-02-19 01:00:23
  * @LastEditors: liusuxian 382185882@qq.com
- * @LastEditTime: 2023-02-19 01:38:01
+ * @LastEditTime: 2023-02-19 01:59:56
  * @FilePath: /playlet-server/Users/liusuxian/Desktop/project-code/golang-project/nova/nnet/tcp_connection.go
  * @Description:
  *
  * Copyright (c) 2023 by ${git_name_email}, All Rights Reserved.
  */
 package nnet
-
-import ()
 
 // Connection 连接类
 type Connection struct {
