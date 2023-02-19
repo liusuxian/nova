9ec5f86f7c304570948b34a2a587950218c84683
liusuxian <382185882@qq.com>
2023-02-19 14:45:50 +0800

    引入gf模块

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/liusuxian/nova
 
 go 1.20
+
+require github.com/gogf/gf/v2 v2.3.2
--- niface/iconnection.go
+++ niface/iconnection.go
@@ -2,7 +2,7 @@
  * @Author: liusuxian 382185882@qq.com
  * @Date: 2023-02-19 00:58:49
  * @LastEditors: liusuxian 382185882@qq.com
- * @LastEditTime: 2023-02-19 01:23:56
+ * @LastEditTime: 2023-02-19 14:44:42
  * @FilePath: /playlet-server/Users/liusuxian/Desktop/project-code/golang-project/nova/niface/iconnection.go
  * @Description:
  *
@@ -20,7 +20,7 @@ type IConnection interface {
 	Start()                                      // 启动连接
 	Stop()                                       // 停止连接
 	Context() context.Context                    // 返回ctx，用于用户自定义的go程获取连接退出状态
-	GetConnection() *net.Conn                    // 从当前连接获取原始的socket Conn
+	GetConnection() net.Conn                     // 从当前连接获取原始的socket Conn
 	GetConnID() uint32                           // 获取当前连接ID
 	RemoteAddr() net.Addr                        // 获取远程客户端地址信息
 	SendMsg(msgID uint32, data []byte) error     // 直接将数据发送给远程的客户端(无缓冲)
