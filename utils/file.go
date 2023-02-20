5a3d7e48797aa88e172420fcceb2f3b5770ac7de
liusuxian <382185882@qq.com>
2023-02-20 17:18:40 +0800

    更新文件工具类方法

--- utils/file.go
+++ utils/file.go
@@ -2,7 +2,7 @@
  * @Author: liusuxian 382185882@qq.com
  * @Date: 2023-02-19 21:04:58
  * @LastEditors: liusuxian 382185882@qq.com
- * @LastEditTime: 2023-02-19 21:06:26
+ * @LastEditTime: 2023-02-20 17:08:17
  * @FilePath: /playlet-server/Users/liusuxian/Desktop/project-code/golang-project/nova/utils/file.go
  * @Description:
  *
@@ -10,10 +10,19 @@
  */
 package utils
 
-import "os"
+import (
+	"os"
+	"path/filepath"
+	"strings"
+)
 
 // PathExists 判断文件或者目录是否存在
 func PathExists(path string) bool {
 	_, err := os.Stat(path)
 	return err == nil || os.IsExist(err)
+}
+
+// ExtName 获取文件扩展名
+func ExtName(path string) string {
+	return strings.TrimLeft(filepath.Ext(path), ".")
 }
