8debe9e638b5c82912b122be6aa799a70679e171
liusuxian <382185882@qq.com>
2023-03-08 14:09:58 +0800

    更新接口

---
+++ niface/iclient.go
@@ -0,0 +1,30 @@
+/*
+ * @Author: liusuxian 382185882@qq.com
+ * @Date: 2023-03-08 13:38:19
+ * @LastEditors: liusuxian 382185882@qq.com
+ * @LastEditTime: 2023-03-08 13:56:36
+ * @FilePath: /playlet-server/Users/liusuxian/Desktop/project-code/golang-project/nova/niface/iclient.go
+ * @Description:
+ *
+ * Copyright (c) 2023 by ${git_name_email}, All Rights Reserved.
+ */
+package niface
+
+import "time"
+
+// IClient 客户端接口
+type IClient interface {
+	Start()                                                   // 启动 Client
+	Stop()                                                    // 停止 Client
+	AddRouter(msgID uint32, router IRouter)                   // 给当前 Client 添加路由
+	Conn() IConnection                                        // 当前 Client 的连接信息
+	SetOnConnStart(func(IConnection))                         // 设置当前 Client 的连接创建时Hook函数
+	SetOnConnStop(func(IConnection))                          // 设置当前 Client 的连接断开时的Hook函数
+	GetOnConnStart() func(IConnection)                        // 获取当前 Client 的连接创建时Hook函数
+	GetOnConnStop() func(IConnection)                         // 获取当前 Client 的连接断开时的Hook函数
+	SetPacket(IDataPack)                                      // 设置当前 Client 绑定的数据协议封包方式
+	GetPacket() IDataPack                                     // 获取当前 Client 绑定的数据协议封包方式
+	GetMsgHandler() IMsgHandle                                // 获取当前 Client 绑定的消息处理模块
+	StartHeartBeat(time.Duration)                             // 启动心跳检测
+	StartHeartBeatWithOption(time.Duration, *HeartBeatOption) // 启动心跳检测(自定义回调)
+}
--- niface/iconnection.go
+++ niface/iconnection.go
@@ -2,7 +2,7 @@
  * @Author: liusuxian 382185882@qq.com
  * @Date: 2023-02-19 00:58:49
  * @LastEditors: liusuxian 382185882@qq.com
- * @LastEditTime: 2023-02-21 18:18:41
+ * @LastEditTime: 2023-03-08 14:08:43
  * @FilePath: /playlet-server/Users/liusuxian/Desktop/project-code/golang-project/nova/niface/iconnection.go
  * @Description:
  *
@@ -19,13 +19,33 @@ import (
 type IConnection interface {
 	Start()                                      // 启动连接
 	Stop()                                       // 停止连接
-	Context() context.Context                    // 返回ctx，用于用户自定义的go程获取连接退出状态
-	GetConnection() net.Conn                     // 从当前连接获取原始的socket Conn
-	GetConnID() uint32                           // 获取当前连接ID
-	RemoteAddr() net.Addr                        // 获取远程客户端地址信息
-	SendMsg(msgID uint32, data []byte) error     // 直接将数据发送给远程的客户端(无缓冲)
-	SendBuffMsg(msgID uint32, data []byte) error // 直接将数据发送给远程的客户端(有缓冲)
-	SetProperty(key string, value any)           // 设置连接属性
-	GetProperty(key string) (any, error)         // 获取连接属性
-	RemoveProperty(key string)                   // 移除连接属性
+	Context() context.Context                    // 返回 ctx，用于用户自定义的 go 程获取连接退出状态
+	GetConnection() net.Conn                     // 从当前连接获取原始的 socket Conn
+	GetConnID() uint32                           // 获取当前连接 ID
+	RemoteAddr() net.Addr                        // 获取当前连接远程地址信息
+	LocalAddr() net.Addr                         // 获取当前连接本地地址信息
+	SendMsg(msgID uint32, data []byte) error     // 直接将 Message 数据发送给远程的客户端(无缓冲)
+	SendBuffMsg(msgID uint32, data []byte) error // 直接将 Message 数据发送给远程的客户端(有缓冲)
+	SetProperty(key string, value interface{})   // 设置当前连接属性
+	GetProperty(key string) (interface{}, error) // 获取当前连接属性
+	RemoveProperty(key string)                   // 移除当前连接属性
+	IsAlive() bool                               // 判断当前连接是否存活
 }
+
+// 用户自定义的心跳检测消息处理方法
+type HeartBeatMsgFunc func(IConnection) []byte
+
+// 用户自定义的远程连接不存活时的处理方法
+type OnRemoteNotAlive func(IConnection)
+
+// HeartBeatOption 心跳检测
+type HeartBeatOption struct {
+	MakeMsg          HeartBeatMsgFunc // 用户自定义的心跳检测消息处理方法
+	OnRemoteNotAlive OnRemoteNotAlive // 用户自定义的远程连接不存活时的处理方法
+	HeadBeatMsgID    uint32           // 用户自定义的心跳检测消息ID
+	Router           IRouter          // 用户自定义的心跳检测消息业务处理路由
+}
+
+const (
+	HeartBeatDefaultMsgID uint32 = 999999 // 默认心跳检测消息ID
+)
