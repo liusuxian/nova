6c4347f8fece5ec6b15e62467a1a27eefbcb6ddf
liusuxian <382185882@qq.com>
2023-04-14 16:34:14 +0800

    新的类型转换函数

---
+++ utils/nconv/conve.go
@@ -0,0 +1,183 @@
+/*
+ * @Author: liusuxian 382185882@qq.com
+ * @Date: 2023-04-14 13:31:56
+ * @LastEditors: liusuxian 382185882@qq.com
+ * @LastEditTime: 2023-04-14 16:31:52
+ * @FilePath: /playlet-server/Users/liusuxian/Desktop/project-code/golang-project/nova/utils/nconv/conve.go
+ * @Description:
+ *
+ * Copyright (c) 2023 by ${git_name_email}, All Rights Reserved.
+ */
+package nconv
+
+import (
+	"encoding/json"
+	"github.com/pkg/errors"
+	"reflect"
+	"strconv"
+	"time"
+)
+
+// ToBoolE 将 any 转换为 bool 类型
+func ToBoolE(i any) (bl bool, err error) {
+	i = indirect(i)
+
+	switch val := i.(type) {
+	case nil:
+		return false, nil
+	case bool:
+		return val, nil
+	case int64:
+		return val > 0, nil
+	case int32:
+		return val > 0, nil
+	case int16:
+		return val > 0, nil
+	case int8:
+		return val > 0, nil
+	case int:
+		return val > 0, nil
+	case uint64:
+		return val > 0, nil
+	case uint32:
+		return val > 0, nil
+	case uint16:
+		return val > 0, nil
+	case uint8:
+		return val > 0, nil
+	case uint:
+		return val > 0, nil
+	case []byte:
+		if len(val) == 0 {
+			return false, nil
+		}
+		iv, err := strconv.ParseBool(string(val))
+		if err == nil {
+			return iv, nil
+		}
+		return false, errors.Errorf("unable to convert %#v of type %T to bool", i, i)
+	case string:
+		if val == "" {
+			return false, nil
+		}
+		iv, err := strconv.ParseBool(val)
+		if err == nil {
+			return iv, nil
+		}
+		return false, errors.Errorf("unable to convert %#v of type %T to bool", i, i)
+	case json.Number:
+		iv, err := ToInt64E(val)
+		if err == nil {
+			return iv > 0, nil
+		}
+		return false, errors.Errorf("unable to convert %#v of type %T to bool", i, i)
+	default:
+		return false, errors.Errorf("unable to convert %#v of type %T to bool", i, i)
+	}
+}
+
+// ToInt64E 将 any 转换为 int64 类型
+func ToInt64E(i any) (iv int64, err error) {
+	i = indirect(i)
+
+	intv, ok := toInt(i)
+	if ok {
+		return int64(intv), nil
+	}
+
+	switch val := i.(type) {
+	case nil:
+		return 0, nil
+	case int64:
+		return val, nil
+	case int32:
+		return int64(val), nil
+	case int16:
+		return int64(val), nil
+	case int8:
+		return int64(val), nil
+	case uint64:
+		return int64(val), nil
+	case uint32:
+		return int64(val), nil
+	case uint16:
+		return int64(val), nil
+	case uint8:
+		return int64(val), nil
+	case uint:
+		return int64(val), nil
+	case float64:
+		return int64(val), nil
+	case float32:
+		return int64(val), nil
+	case bool:
+		if val {
+			return 1, nil
+		}
+		return 0, nil
+	case []byte:
+		v, err := strconv.ParseInt(trimZeroDecimal(string(val)), 0, 0)
+		if err == nil {
+			return v, nil
+		}
+		return 0, errors.Errorf("unable to convert %#v of type %T to int64", i, i)
+	case string:
+		v, err := strconv.ParseInt(trimZeroDecimal(val), 0, 0)
+		if err == nil {
+			return v, nil
+		}
+		return 0, errors.Errorf("unable to convert %#v of type %T to int64", i, i)
+	case json.Number:
+		return ToInt64E(string(val))
+	default:
+		return 0, errors.Errorf("unable to convert %#v of type %T to int64", i, i)
+	}
+}
+
+// indirect 对给定的值进行多次解引用以达到基本类型（或 nil）
+func indirect(i any) (iv any) {
+	if i == nil {
+		return nil
+	}
+	if t := reflect.TypeOf(i); t.Kind() != reflect.Ptr {
+		// 如果不是指针类型，避免创建 reflect.Value
+		return i
+	}
+	v := reflect.ValueOf(i)
+	for v.Kind() == reflect.Ptr && !v.IsNil() {
+		v = v.Elem()
+	}
+	return v.Interface()
+}
+
+// toInt 如果 i 或 i 的底层类型是 int，则返回 i 的 int 值
+func toInt(i any) (iv int, bl bool) {
+	switch i := i.(type) {
+	case int:
+		return i, true
+	case time.Weekday:
+		return int(i), true
+	case time.Month:
+		return int(i), true
+	default:
+		return 0, false
+	}
+}
+
+// trimZeroDecimal 删除字符串中末尾的零和小数点
+func trimZeroDecimal(s string) (v string) {
+	var foundZero bool
+	for i := len(s); i > 0; i-- {
+		switch s[i-1] {
+		case '.':
+			if foundZero {
+				return s[:i-1]
+			}
+		case '0':
+			foundZero = true
+		default:
+			return s
+		}
+	}
+	return s
+}
---
+++ utils/nconv/conve_test.go
@@ -0,0 +1,351 @@
+/*
+ * @Author: liusuxian 382185882@qq.com
+ * @Date: 2023-04-14 13:31:56
+ * @LastEditors: liusuxian 382185882@qq.com
+ * @LastEditTime: 2023-04-14 16:33:24
+ * @FilePath: /playlet-server/Users/liusuxian/Desktop/project-code/golang-project/nova/utils/nconv/conve_test.go
+ * @Description:
+ *
+ * Copyright (c) 2023 by ${git_name_email}, All Rights Reserved.
+ */
+package nconv_test
+
+import (
+	"github.com/liusuxian/nova/utils/nconv"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func errLog(t *testing.T, err error) {
+	if err != nil {
+		t.Logf("Error: %+v\n", err)
+	}
+}
+
+func TestToBoolE(t *testing.T) {
+	assert := assert.New(t)
+	actualObj, err := nconv.ToBoolE(nil) // nil
+	errLog(t, err)
+	if assert.NoError(err) {
+		assert.False(actualObj)
+	}
+	actualObj, err = nconv.ToBoolE(true) // bool
+	errLog(t, err)
+	if assert.NoError(err) {
+		assert.True(actualObj)
+	}
+	actualObj, err = nconv.ToBoolE(false) // bool
+	errLog(t, err)
+	if assert.NoError(err) {
+		assert.False(actualObj)
+	}
+	actualObj, err = nconv.ToBoolE(int64(0)) // int64
+	errLog(t, err)
+	if assert.NoError(err) {
+		assert.False(actualObj)
+	}
+	actualObj, err = nconv.ToBoolE(int64(-1)) // int64
+	errLog(t, err)
+	if assert.NoError(err) {
+		assert.False(actualObj)
+	}
+	actualObj, err = nconv.ToBoolE(int64(1)) // int64
+	errLog(t, err)
+	if assert.NoError(err) {
+		assert.True(actualObj)
+	}
+	actualObj, err = nconv.ToBoolE(int32(0)) // int32
+	errLog(t, err)
+	if assert.NoError(err) {
+		assert.False(actualObj)
+	}
+	actualObj, err = nconv.ToBoolE(int32(-1)) // int32
+	errLog(t, err)
+	if assert.NoError(err) {
+		assert.False(actualObj)
+	}
+	actualObj, err = nconv.ToBoolE(int32(1)) // int32
+	errLog(t, err)
+	if assert.NoError(err) {
+		assert.True(actualObj)
+	}
+	actualObj, err = nconv.ToBoolE(int16(0)) // int16
+	errLog(t, err)
+	if assert.NoError(err) {
+		assert.False(actualObj)
+	}
+	actualObj, err = nconv.ToBoolE(int16(-1)) // int16
+	errLog(t, err)
+	if assert.NoError(err) {
+		assert.False(actualObj)
+	}
+	actualObj, err = nconv.ToBoolE(int16(1)) // int16
+	errLog(t, err)
+	if assert.NoError(err) {
+		assert.True(actualObj)
+	}
+	actualObj, err = nconv.ToBoolE(int8(0)) // int8
+	errLog(t, err)
+	if assert.NoError(err) {
+		assert.False(actualObj)
+	}
+	actualObj, err = nconv.ToBoolE(int8(-1)) // int8
+	errLog(t, err)
+	if assert.NoError(err) {
+		assert.False(actualObj)
+	}
+	actualObj, err = nconv.ToBoolE(int8(1)) // int8
+	errLog(t, err)
+	if assert.NoError(err) {
+		assert.True(actualObj)
+	}
+	actualObj, err = nconv.ToBoolE(byte('a')) // int8
+	errLog(t, err)
+	if assert.NoError(err) {
+		assert.True(actualObj)
+	}
+	actualObj, err = nconv.ToBoolE(0) // int
+	errLog(t, err)
+	if assert.NoError(err) {
+		assert.False(actualObj)
+	}
+	actualObj, err = nconv.ToBoolE(-1) // int
+	errLog(t, err)
+	if assert.NoError(err) {
+		assert.False(actualObj)
+	}
+	actualObj, err = nconv.ToBoolE(1) // int
+	errLog(t, err)
+	if assert.NoError(err) {
+		assert.True(actualObj)
+	}
+	actualObj, err = nconv.ToBoolE(uint64(0)) // uint64
+	errLog(t, err)
+	if assert.NoError(err) {
+		assert.False(actualObj)
+	}
+	actualObj, err = nconv.ToBoolE(uint64(1)) // uint64
+	errLog(t, err)
+	if assert.NoError(err) {
+		assert.True(actualObj)
+	}
+	actualObj, err = nconv.ToBoolE(uint32(0)) // uint32
+	errLog(t, err)
+	if assert.NoError(err) {
+		assert.False(actualObj)
+	}
+	actualObj, err = nconv.ToBoolE(uint32(1)) // uint32
+	errLog(t, err)
+	if assert.NoError(err) {
+		assert.True(actualObj)
+	}
+	actualObj, err = nconv.ToBoolE(uint16(0)) // uint16
+	errLog(t, err)
+	if assert.NoError(err) {
+		assert.False(actualObj)
+	}
+	actualObj, err = nconv.ToBoolE(uint16(1)) // uint16
+	errLog(t, err)
+	if assert.NoError(err) {
+		assert.True(actualObj)
+	}
+	actualObj, err = nconv.ToBoolE(uint8(0)) // uint8
+	errLog(t, err)
+	if assert.NoError(err) {
+		assert.False(actualObj)
+	}
+	actualObj, err = nconv.ToBoolE(uint8(1)) // uint8
+	errLog(t, err)
+	if assert.NoError(err) {
+		assert.True(actualObj)
+	}
+	actualObj, err = nconv.ToBoolE(byte('A')) // uint8
+	errLog(t, err)
+	if assert.NoError(err) {
+		assert.True(actualObj)
+	}
+	actualObj, err = nconv.ToBoolE(uint(0)) // uint
+	errLog(t, err)
+	if assert.NoError(err) {
+		assert.False(actualObj)
+	}
+	actualObj, err = nconv.ToBoolE(uint(1)) // uint
+	errLog(t, err)
+	if assert.NoError(err) {
+		assert.True(actualObj)
+	}
+	actualObj, err = nconv.ToBoolE([]byte("true")) // []byte
+	errLog(t, err)
+	if assert.NoError(err) {
+		assert.True(actualObj)
+	}
+	actualObj, err = nconv.ToBoolE([]byte("")) // []byte
+	errLog(t, err)
+	if assert.NoError(err) {
+		assert.False(actualObj)
+	}
+	actualObj, err = nconv.ToBoolE([]byte{}) // []byte
+	errLog(t, err)
+	if assert.NoError(err) {
+		assert.False(actualObj)
+	}
+	actualObj, err = nconv.ToBoolE("true") // string
+	errLog(t, err)
+	if assert.NoError(err) {
+		assert.True(actualObj)
+	}
+	actualObj, err = nconv.ToBoolE("false") // string
+	errLog(t, err)
+	if assert.NoError(err) {
+		assert.False(actualObj)
+	}
+	actualObj, err = nconv.ToBoolE("") // string
+	errLog(t, err)
+	if assert.NoError(err) {
+		assert.False(actualObj)
+	}
+	actualObj, err = nconv.ToBoolE(" ") // string
+	errLog(t, err)
+	if assert.Error(err) {
+		assert.False(actualObj)
+	}
+	actualObj, err = nconv.ToBoolE("0") // string
+	errLog(t, err)
+	if assert.NoError(err) {
+		assert.False(actualObj)
+	}
+	actualObj, err = nconv.ToBoolE("hello") // string
+	errLog(t, err)
+	if assert.Error(err) {
+		assert.False(actualObj)
+	}
+}
+
+func TestToInt64E(t *testing.T) {
+	assert := assert.New(t)
+	actualObj, err := nconv.ToInt64E(nil) // nil
+	errLog(t, err)
+	if assert.NoError(err) {
+		assert.Equal(int64(0), actualObj)
+	}
+	actualObj, err = nconv.ToInt64E(int64(1)) // int64
+	errLog(t, err)
+	if assert.NoError(err) {
+		assert.Equal(int64(1), actualObj)
+	}
+	actualObj, err = nconv.ToInt64E(int32(1)) // int32
+	errLog(t, err)
+	if assert.NoError(err) {
+		assert.Equal(int64(1), actualObj)
+	}
+	actualObj, err = nconv.ToInt64E(int16(1)) // int16
+	errLog(t, err)
+	if assert.NoError(err) {
+		assert.Equal(int64(1), actualObj)
+	}
+	actualObj, err = nconv.ToInt64E(int8(1)) // int8
+	errLog(t, err)
+	if assert.NoError(err) {
+		assert.Equal(int64(1), actualObj)
+	}
+	actualObj, err = nconv.ToInt64E(int(1)) // int
+	errLog(t, err)
+	if assert.NoError(err) {
+		assert.Equal(int64(1), actualObj)
+	}
+	actualObj, err = nconv.ToInt64E(uint64(1)) // uint64
+	errLog(t, err)
+	if assert.NoError(err) {
+		assert.Equal(int64(1), actualObj)
+	}
+	actualObj, err = nconv.ToInt64E(uint32(1)) // uint32
+	errLog(t, err)
+	if assert.NoError(err) {
+		assert.Equal(int64(1), actualObj)
+	}
+	actualObj, err = nconv.ToInt64E(uint16(1)) // uint16
+	errLog(t, err)
+	if assert.NoError(err) {
+		assert.Equal(int64(1), actualObj)
+	}
+	actualObj, err = nconv.ToInt64E(uint8(1)) // uint8
+	errLog(t, err)
+	if assert.NoError(err) {
+		assert.Equal(int64(1), actualObj)
+	}
+	actualObj, err = nconv.ToInt64E(uint(1)) // uint
+	errLog(t, err)
+	if assert.NoError(err) {
+		assert.Equal(int64(1), actualObj)
+	}
+	actualObj, err = nconv.ToInt64E(float64(1.23)) // float64
+	errLog(t, err)
+	if assert.NoError(err) {
+		assert.Equal(int64(1), actualObj)
+	}
+	actualObj, err = nconv.ToInt64E(float64(1.56)) // float64
+	errLog(t, err)
+	if assert.NoError(err) {
+		assert.Equal(int64(1), actualObj)
+	}
+	actualObj, err = nconv.ToInt64E(float32(1.23)) // float32
+	errLog(t, err)
+	if assert.NoError(err) {
+		assert.Equal(int64(1), actualObj)
+	}
+	actualObj, err = nconv.ToInt64E(float32(1.56)) // float32
+	errLog(t, err)
+	if assert.NoError(err) {
+		assert.Equal(int64(1), actualObj)
+	}
+	actualObj, err = nconv.ToInt64E(true) // bool
+	errLog(t, err)
+	if assert.NoError(err) {
+		assert.Equal(int64(1), actualObj)
+	}
+	actualObj, err = nconv.ToInt64E(false) // bool
+	errLog(t, err)
+	if assert.NoError(err) {
+		assert.Equal(int64(0), actualObj)
+	}
+	actualObj, err = nconv.ToInt64E([]byte("1.23")) // []byte
+	errLog(t, err)
+	if assert.Error(err) {
+		assert.Equal(int64(0), actualObj)
+	}
+	actualObj, err = nconv.ToInt64E([]byte("1.0")) // []byte
+	errLog(t, err)
+	if assert.NoError(err) {
+		assert.Equal(int64(1), actualObj)
+	}
+	actualObj, err = nconv.ToInt64E([]byte("1.")) // []byte
+	errLog(t, err)
+	if assert.Error(err) {
+		assert.Equal(int64(0), actualObj)
+	}
+	actualObj, err = nconv.ToInt64E([]byte("1")) // []byte
+	errLog(t, err)
+	if assert.NoError(err) {
+		assert.Equal(int64(1), actualObj)
+	}
+	actualObj, err = nconv.ToInt64E("1.23") // string
+	errLog(t, err)
+	if assert.Error(err) {
+		assert.Equal(int64(0), actualObj)
+	}
+	actualObj, err = nconv.ToInt64E("1.0") // string
+	errLog(t, err)
+	if assert.NoError(err) {
+		assert.Equal(int64(1), actualObj)
+	}
+	actualObj, err = nconv.ToInt64E("1.") // string
+	errLog(t, err)
+	if assert.Error(err) {
+		assert.Equal(int64(0), actualObj)
+	}
+	actualObj, err = nconv.ToInt64E("1") // string
+	errLog(t, err)
+	if assert.NoError(err) {
+		assert.Equal(int64(1), actualObj)
+	}
+}
