5bffbb6db75626287e0b41d948b60c6aaee7fd75
liusuxian <382185882@qq.com>
2023-04-16 01:12:21 +0800

    单元测试

--- utils/nconv/conve_string_test.go
+++ utils/nconv/conve_string_test.go
@@ -2,7 +2,7 @@
  * @Author: liusuxian 382185882@qq.com
  * @Date: 2023-04-15 13:27:35
  * @LastEditors: liusuxian 382185882@qq.com
- * @LastEditTime: 2023-04-15 13:42:38
+ * @LastEditTime: 2023-04-16 01:11:49
  * @FilePath: /playlet-server/Users/liusuxian/Desktop/project-code/golang-project/nova/utils/nconv/conve_string_test.go
  * @Description:
  *
@@ -16,6 +16,13 @@ import (
 	"testing"
 	"time"
 )
+
+type A struct {
+	A int
+	B float64
+	C string
+	D []any
+}
 
 func TestToStringE(t *testing.T) {
 	assert := assert.New(t)
@@ -58,5 +65,15 @@ func TestToStringE(t *testing.T) {
 	errLog(t, err)
 	if assert.NoError(err) {
 		assert.Equal("[1,1.2,\"hello\",true]", actualObj)
+	}
+	actualObj, err = nconv.ToStringE(map[string]any{"a": 1, "b": 1}) // map[string]any
+	errLog(t, err)
+	if assert.NoError(err) {
+		assert.Equal("{\"a\":1,\"b\":1}", actualObj)
+	}
+	actualObj, err = nconv.ToStringE(&A{A: 1, B: 1.2, C: "hello", D: []any{1, 1.2, "hello", true}}) // struct
+	errLog(t, err)
+	if assert.NoError(err) {
+		assert.Equal("{\"A\":1,\"B\":1.2,\"C\":\"hello\",\"D\":[1,1.2,\"hello\",true]}", actualObj)
 	}
 }
