9228a0e4813b73fe0df5cf076d7f2d4fdaf2b49c
liusuxian <382185882@qq.com>
2023-04-18 18:27:08 +0800

    修复H5剧列表展示bug

--- utils/nconv/decoder.go
+++ utils/nconv/decoder.go
@@ -2,7 +2,7 @@
  * @Author: liusuxian 382185882@qq.com
  * @Date: 2023-04-18 13:07:45
  * @LastEditors: liusuxian 382185882@qq.com
- * @LastEditTime: 2023-04-18 14:07:09
+ * @LastEditTime: 2023-04-18 18:26:26
  * @FilePath: /playlet-server/Users/liusuxian/Desktop/project-code/golang-project/nova/utils/nconv/decoder.go
  * @Description:
  *
@@ -14,6 +14,42 @@ import "github.com/mitchellh/mapstructure"
 
 // DecoderConfigOption 解码配置选项
 type DecoderConfigOption func(*mapstructure.DecoderConfig)
+
+// WithDecoderConfigOption
+func WithDecoderConfigOption(config *mapstructure.DecoderConfig) DecoderConfigOption {
+	return func(c *mapstructure.DecoderConfig) {
+		if config.DecodeHook != nil {
+			c.DecodeHook = config.DecodeHook
+		}
+		if c.ErrorUnused != config.ErrorUnused {
+			c.ErrorUnused = config.ErrorUnused
+		}
+		if c.ErrorUnset != config.ErrorUnset {
+			c.ErrorUnset = config.ErrorUnset
+		}
+		if c.ZeroFields != config.ZeroFields {
+			c.ZeroFields = config.ZeroFields
+		}
+		if c.WeaklyTypedInput != config.WeaklyTypedInput {
+			c.WeaklyTypedInput = config.WeaklyTypedInput
+		}
+		if c.Squash != config.Squash {
+			c.Squash = config.Squash
+		}
+		if config.Metadata != nil {
+			c.Metadata = config.Metadata
+		}
+		if config.TagName != "" && c.TagName != config.TagName {
+			c.TagName = config.TagName
+		}
+		if c.IgnoreUntaggedFields != config.IgnoreUntaggedFields {
+			c.IgnoreUntaggedFields = config.IgnoreUntaggedFields
+		}
+		if config.MatchName != nil {
+			c.MatchName = config.MatchName
+		}
+	}
+}
 
 // decode 解码
 func decode(input any, config *mapstructure.DecoderConfig) (err error) {
@@ -27,14 +63,15 @@ func decode(input any, config *mapstructure.DecoderConfig) (err error) {
 // defaultDecoderConfig 默认的解码配置
 func defaultDecoderConfig(output any, opts ...DecoderConfigOption) (config *mapstructure.DecoderConfig) {
 	c := &mapstructure.DecoderConfig{
-		Metadata:         nil,
-		Result:           output,
-		WeaklyTypedInput: true,
 		DecodeHook: mapstructure.ComposeDecodeHookFunc(
 			mapstructure.StringToTimeDurationHookFunc(),
 			mapstructure.StringToSliceHookFunc(","),
 			mapstructure.RecursiveStructToMapHookFunc(),
 		),
+		WeaklyTypedInput: true,
+		Metadata:         nil,
+		Result:           output,
+		TagName:          "json",
 	}
 	for _, opt := range opts {
 		opt(c)
