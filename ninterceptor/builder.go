662d25b14c2ed2e1b0d2e9b3cd9155070a6fc2e1
liusuxian <382185882@qq.com>
2023-05-06 18:48:12 +0800

    责任链模式修改优化

--- ninterceptor/builder.go
+++ ninterceptor/builder.go
@@ -12,37 +12,37 @@ package ninterceptor
 
 import "github.com/liusuxian/nova/niface"
 
-// chainBuilder 责任链构造器结构
-type chainBuilder struct {
+// ChainBuilder 责任链构造器结构
+type ChainBuilder struct {
 	head niface.IInterceptor   // 责任链头
 	tail niface.IInterceptor   // 责任链尾
 	body []niface.IInterceptor // 责任链主体
 }
 
-// newChainBuilder 创建责任链构造器实例
-func newChainBuilder() (builder *chainBuilder) {
-	return &chainBuilder{
+// NewChainBuilder 创建责任链构造器实例
+func NewChainBuilder() (builder *ChainBuilder) {
+	return &ChainBuilder{
 		body: make([]niface.IInterceptor, 0),
 	}
 }
 
 // Head 将拦截器添加到责任链的开头
-func (ic *chainBuilder) Head(interceptor niface.IInterceptor) {
+func (ic *ChainBuilder) Head(interceptor niface.IInterceptor) {
 	ic.head = interceptor
 }
 
 // Tail 将拦截器添加到责任链的尾部
-func (ic *chainBuilder) Tail(interceptor niface.IInterceptor) {
+func (ic *ChainBuilder) Tail(interceptor niface.IInterceptor) {
 	ic.tail = interceptor
 }
 
 // AddInterceptor 将拦截器添加到责任链的主体中
-func (ic *chainBuilder) AddInterceptor(interceptor niface.IInterceptor) {
+func (ic *ChainBuilder) AddInterceptor(interceptor niface.IInterceptor) {
 	ic.body = append(ic.body, interceptor)
 }
 
 // Execute 按顺序执行当前责任链中的所有拦截器
-func (ic *chainBuilder) Execute(req niface.IcReq) (resp niface.IcResp) {
+func (ic *ChainBuilder) Execute(req niface.IcReq) (resp niface.IcResp) {
 	// 将所有拦截器放入责任链构造器中
 	var interceptors []niface.IInterceptor
 	if ic.head != nil {
