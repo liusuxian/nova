4c8350611f5f5f26a7dc7a86430039ca02ee98c0
liusuxian <382185882@qq.com>
2023-05-06 17:25:04 +0800

    路由修改优化

--- niface/router.go
+++ niface/router.go
@@ -2,7 +2,7 @@
  * @Author: liusuxian 382185882@qq.com
  * @Date: 2023-02-19 01:27:08
  * @LastEditors: liusuxian 382185882@qq.com
- * @LastEditTime: 2023-03-15 20:14:46
+ * @LastEditTime: 2023-05-06 17:21:51
  * @FilePath: /playlet-server/Users/liusuxian/Desktop/project-code/golang-project/nova/niface/router.go
  * @Description:
  *
@@ -10,9 +10,19 @@
  */
 package niface
 
+// RouterHandler 路由处理函数
+type RouterHandler func(request IRequest)
+
 // IRouter 路由接口
 type IRouter interface {
-	PreHandle(request IRequest)  // 在处理 Connection 业务之前的钩子
-	Handle(request IRequest)     // 处理 Connection 业务
-	PostHandle(request IRequest) // 处理 Connection 业务之后的钩子
+	Use(handlers ...RouterHandler)                                                     // 添加全局路由
+	AddHandler(msgID uint16, handlers ...RouterHandler)                                // 添加路由
+	Group(startMsgID, endMsgID uint16, handlers ...RouterHandler) (group IGroupRouter) // 路由组管理
+	GetHandlers(msgID uint16) (handlers []RouterHandler, isExist bool)                 // 获取路由处理函数集合
+}
+
+// IGroupRouter 路由组接口
+type IGroupRouter interface {
+	Use(handlers ...RouterHandler)                      // 添加全局路由
+	AddHandler(msgID uint16, handlers ...RouterHandler) // 添加路由
 }
--- nrouter/router.go
+++ nrouter/router.go
@@ -2,7 +2,7 @@
  * @Author: liusuxian 382185882@qq.com
  * @Date: 2023-03-08 15:22:54
  * @LastEditors: liusuxian 382185882@qq.com
- * @LastEditTime: 2023-03-13 21:27:50
+ * @LastEditTime: 2023-05-06 17:19:47
  * @FilePath: /playlet-server/Users/liusuxian/Desktop/project-code/golang-project/nova/nrouter/router.go
  * @Description:
  *
@@ -10,27 +10,92 @@
  */
 package nrouter
 
-import "github.com/liusuxian/nova/niface"
+import (
+	"github.com/liusuxian/nova/niface"
+	"strconv"
+	"sync"
+)
 
-// BaseRouter 基础路由结构
-// 实现 Router 时，先嵌入这个结构，然后根据需要对这个结构的方法进行重写
-type BaseRouter struct {
+// Router 路由结构
+type Router struct {
+	apis         map[uint16][]niface.RouterHandler // 存放每个 MsgID 所对应的路由处理函数集合
+	handlers     []niface.RouterHandler            // 存放全局路由处理函数集合
+	sync.RWMutex                                   // 并发读写锁
 }
 
-// 之所以 BaseRouter 的方法都为空，是因为有的 Router 不希望有 PreHandle 或 PostHandle
-// 所以 Router 全部复合 BaseRouter 的好处是，不需要实现 PreHandle 和 PostHandle 也可以实例化
-
-// PreHandle 在处理 Connection 业务之前的钩子
-func (br *BaseRouter) PreHandle(request niface.IRequest) {
-
+// GroupRouter 路由组结构
+type GroupRouter struct {
+	startMsgID uint16                 // 起始 MsgID
+	endMsgID   uint16                 // 终止 MsgID
+	handlers   []niface.RouterHandler // 存放全局路由处理函数集合
+	router     niface.IRouter         // 路由
 }
 
-// Handle 处理 Connection 业务
-func (br *BaseRouter) Handle(request niface.IRequest) {
-
+// NewRouter 创建路由
+func NewRouter() (r *Router) {
+	return &Router{
+		apis:     make(map[uint16][]niface.RouterHandler, 10),
+		handlers: make([]niface.RouterHandler, 0, 6),
+	}
 }
 
-// PostHandle 处理 Connection 业务之后的钩子
-func (br *BaseRouter) PostHandle(request niface.IRequest) {
+// NewGroupRouter 创建路由组
+func NewGroupRouter(startMsgID, endMsgID uint16, router *Router, handlers ...niface.RouterHandler) (group *GroupRouter) {
+	group = &GroupRouter{
+		startMsgID: startMsgID,
+		endMsgID:   endMsgID,
+		handlers:   make([]niface.RouterHandler, 0, len(handlers)),
+		router:     router,
+	}
+	group.handlers = append(group.handlers, handlers...)
+	return
+}
 
+// Use 添加全局路由
+func (r *Router) Use(handlers ...niface.RouterHandler) {
+	r.handlers = append(r.handlers, handlers...)
+}
+
+// AddHandler 添加路由
+func (r *Router) AddHandler(msgID uint16, handlers ...niface.RouterHandler) {
+	if _, ok := r.apis[msgID]; ok {
+		panic("Repeated Api MsgID = " + strconv.Itoa(int(msgID)))
+	}
+
+	length := len(r.handlers) + len(handlers)
+	newHandlers := make([]niface.RouterHandler, length)
+	copy(newHandlers, r.handlers)
+	copy(newHandlers[len(r.handlers):], handlers)
+	r.apis[msgID] = append(r.apis[msgID], newHandlers...)
+}
+
+// Group 路由组管理
+func (r *Router) Group(startMsgID uint16, endMsgID uint16, handlers ...niface.RouterHandler) (group niface.IGroupRouter) {
+	return NewGroupRouter(startMsgID, endMsgID, r, handlers...)
+}
+
+// GetHandlers 获取路由处理函数集合
+func (r *Router) GetHandlers(msgID uint16) (handlers []niface.RouterHandler, isExist bool) {
+	r.RLock()
+	defer r.RUnlock()
+	handlers, isExist = r.apis[msgID]
+	return
+}
+
+// Use 添加全局路由
+func (gr *GroupRouter) Use(handlers ...niface.RouterHandler) {
+	gr.handlers = append(gr.handlers, handlers...)
+}
+
+// AddHandler 添加路由
+func (gr *GroupRouter) AddHandler(msgID uint16, handlers ...niface.RouterHandler) {
+	if msgID < gr.startMsgID || msgID > gr.endMsgID {
+		panic("Add Router To Group Error In MsgID: " + strconv.Itoa(int(msgID)))
+	}
+
+	length := len(gr.handlers) + len(handlers)
+	newHandlers := make([]niface.RouterHandler, length)
+	copy(newHandlers, gr.handlers)
+	copy(newHandlers[len(gr.handlers):], handlers)
+	gr.router.AddHandler(msgID, newHandlers...)
 }
