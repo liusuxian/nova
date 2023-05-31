550f89a45df23b3cd1b78eb94cc9cae9cb9e89a3
liusuxian <382185882@qq.com>
2023-05-31 15:40:48 +0800

    通知接口新增保存和获取离线消息接口的接口

--- niface/notify.go
+++ niface/notify.go
@@ -2,25 +2,33 @@
  * @Author: liusuxian 382185882@qq.com
  * @Date: 2023-03-31 17:25:31
  * @LastEditors: liusuxian 382185882@qq.com
- * @LastEditTime: 2023-05-31 01:39:24
+ * @LastEditTime: 2023-05-31 15:09:50
  * @Description:
  *
  * Copyright (c) 2023 by liusuxian email: 382185882@qq.com, All Rights Reserved.
  */
 package niface
 
-// IOfflineMsg 离线消息接口
-type IOfflineMsg interface {
-	Save() (err error) // 保存离线消息
-}
-
 // INotify 通知接口
 type INotify interface {
-	HasID(id uint32) (has bool)                                                                      // 是否存在这个 ID
-	ConnCount() (count int)                                                                          // 连接的数量
-	AddNotifyID(id uint32, conn IConnection)                                                         // 添加连接
-	RemoveNotifyID(id uint32)                                                                        // 删除连接
-	GetNotifyConn(id uint32) (conn IConnection, isExist bool)                                        // 获取连接
-	Notify(id uint32, msgID uint16, f MsgDataFunc, offlineMsg IOfflineMsg, callback ...SendCallback) // 通知连接
-	NotifyAll(msgID uint16, f MsgDataFunc, callback ...SendCallback)                                 // 通知所有连接
+	HasID(id uint32) (has bool)                                                                                    // 是否存在这个 ID
+	ConnCount() (count int)                                                                                        // 连接的数量
+	AddNotifyID(id uint32, conn IConnection)                                                                       // 添加连接
+	RemoveNotifyID(id uint32)                                                                                      // 删除连接
+	GetNotifyConn(id uint32) (conn IConnection, isExist bool)                                                      // 获取连接
+	Notify(id uint32, msgID uint16, f MsgDataFunc, callback ...SendCallback)                                       // 通知连接
+	NotifyAll(msgID uint16, f MsgDataFunc, callback ...SendCallback)                                               // 通知所有连接
+	NotifySaveOfflineMsg(id uint32, msgID uint16, f MsgDataFunc, offlineMsg IOfflineMsg, callback ...SendCallback) // 通知连接，当连接不存在或处于关闭状态时保存离线消息
+}
+
+// IOfflineMsg 离线消息接口
+type IOfflineMsg interface {
+	Save(id uint32, msgID uint16, f MsgDataFunc) (err error) // 保存离线消息
+	Get(id uint32) (msgList []OfflineMsg, err error)         // 获取离线消息
+}
+
+// OfflineMsg 离线消息
+type OfflineMsg struct {
+	MsgID   uint16 // 消息ID
+	MsgData []byte // 消息内容
 }
--- nnotify/notify.go
+++ nnotify/notify.go
@@ -2,7 +2,7 @@
  * @Author: liusuxian 382185882@qq.com
  * @Date: 2023-05-27 01:28:36
  * @LastEditors: liusuxian 382185882@qq.com
- * @LastEditTime: 2023-05-31 01:29:55
+ * @LastEditTime: 2023-05-31 15:37:40
  * @Description:
  *
  * Copyright (c) 2023 by liusuxian email: 382185882@qq.com, All Rights Reserved.
@@ -67,16 +67,10 @@ func (n *Notify) GetNotifyConn(id uint32) (conn niface.IConnection, isExist bool
 }
 
 // Notify 通知连接
-func (n *Notify) Notify(id uint32, msgID uint16, f niface.MsgDataFunc, offlineMsg niface.IOfflineMsg, callback ...niface.SendCallback) {
+func (n *Notify) Notify(id uint32, msgID uint16, f niface.MsgDataFunc, callback ...niface.SendCallback) {
 	// 获取连接
 	conn, isExist := n.GetNotifyConn(id)
 	if !isExist {
-		if offlineMsg != nil {
-			if err := offlineMsg.Save(); err != nil {
-				nlog.Error("Save Offline Msg Error", nlog.Err(err))
-				return
-			}
-		}
 		return
 	}
 	// 将 Message 数据发送给远程的对端
@@ -98,5 +92,26 @@ func (n *Notify) NotifyAll(msgID uint16, f niface.MsgDataFunc, callback ...nifac
 		if err := conn.SendMsg(msgID, f, callback...); err != nil && err != nerr.ErrConnectionClosed {
 			nlog.Error("NotifyAll Error", nlog.Err(err))
 		}
+	}
+}
+
+// NotifySaveOfflineMsg 通知连接，当连接不存在或处于关闭状态时保存离线消息
+func (n *Notify) NotifySaveOfflineMsg(id uint32, msgID uint16, f niface.MsgDataFunc, offlineMsg niface.IOfflineMsg, callback ...niface.SendCallback) {
+	// 获取连接
+	conn, isExist := n.GetNotifyConn(id)
+	if !isExist {
+		if offlineMsg != nil {
+			// 保存离线消息
+			if err := offlineMsg.Save(id, msgID, f); err != nil {
+				nlog.Error("Save Offline Msg Error", nlog.Err(err))
+				return
+			}
+		}
+		return
+	}
+	// 将 Message 数据发送给远程的对端
+	if err := conn.SendMsg(msgID, f, callback...); err != nil && err != nerr.ErrConnectionClosed {
+		nlog.Error("Notify Error", nlog.Err(err))
+		return
 	}
 }
